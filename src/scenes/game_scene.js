8d6b1f1618215405739380568e0e5e4cb5fb5fad
Ittalian <bothmather@au.com>
2024-10-18 18:14:10 +0900

    デバッグモード、ログ出力

--- src/scenes/game_scene.js
+++ src/scenes/game_scene.js
@@ -9,5 +9,17 @@ export function gameScene() {
       k.area(),
       k.offscreen({ destroy: true }),
     ]);
+
+    k.add([
+      k.rect(50, 50),
+      k.pos(50, 100),
+      k.move(RIGHT, 50),
+      'player'
+    ]);
+
+    k.onUpdate('player', p => {
+      // ログの出力(console.logみたいな感じ)
+      // k.debug.log(p.pos.x);
+    });
   });
 }
